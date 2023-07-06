a714bdc46e8d753699080aa2060abc1e69f3fd85
Raphael Forment <raphael.forment@gmail.com>
2023-07-06 11:18:43 +0200

    Transport

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import { Application } from './Application.js';
 import type { SavedContext } from './Application.js';
 
+let RUNNING: boolean = false;
+
 let application: Application = new Application('text');
 let cursor: HTMLElement = document.getElementById("cursor");
 let universe: HTMLElement = document.getElementById("universe");
 let prompt: HTMLElement = document.getElementById("prompt");
-
-
+let play_button: HTMLElement = document.getElementById("play"); 
 let zone: HTMLElement = document.getElementById("zone");
 
-
-function start() {
+play_button.addEventListener("click", () => {
+    if (!RUNNING) {
     application.startAudioContext();
-}
+    play_button.innerHTML = "⏸";
+    RUNNING = !RUNNING;
+    } else {
+    play_button.innerHTML = "⏵";
+    application.audio_context.suspend();
+    RUNNING = !RUNNING;
+    }
+}); 
+
 
 function drawScreen() {
     return application.process();
