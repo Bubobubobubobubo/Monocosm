b8914ac491d1b40d70c1de06fc944c00af45866e
Raphael Forment <raphael.forment@gmail.com>
2023-07-09 11:28:10 +0200

    Add some ToneJS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Application } from './Application.js';
 import type { SavedContext } from './Types.js';
+import * as Tone from 'tone';
 
 let application: Application = new Application('text');
 let cursor: HTMLElement = document.getElementById("cursor") as HTMLElement;
@@ -8,13 +9,14 @@ let play_button: HTMLElement = document.getElementById("play") as HTMLElement;
 let zone: HTMLElement = document.getElementById("zone") as HTMLElement;
 let clock: HTMLElement = document.getElementById("clock") as HTMLElement;
 
-play_button.addEventListener("click", () => {
+play_button.addEventListener("click", async () => {
     if (!application.running) {
-    application.startAudioContext();
+    // application.startAudioContext();
+    await Tone.start();
     play_button.textContent = "⏸";
     } else {
     play_button.textContent = "⏵";
-    application.audio_context.suspend();
+    // application.audio_context.suspend();
     }
     application.running= !application;
 }); 
